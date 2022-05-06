e2bfaf9d724cad53c1001c44dbed335ac21ccc7b
SangKyoungPark <tltmzh3@gmail.com>
2022-05-06 22:33:39 +0900

    [Commit] Hello World!

--- C_Sharp_Study/Program.cs
+++ C_Sharp_Study/Program.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C_Sharp_Study
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace C_Sharp_Study {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
         }
     }
 }
