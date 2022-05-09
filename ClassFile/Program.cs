e2db65d301dfd5dfe9a41ef519479626c1d9524c
SangKyoungPark <tltmzh3@gmail.com>
2022-05-09 11:31:48 +0900

    [Commit] ClassFile

---
+++ ClassFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFile
+{
+    //컨트롤 k f 자동코드 정렬
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FightUnit NewPalyer = new FightUnit();
+        }
+    }
+}
