288dc37b6d8f2bf879c617d001358af0b03d5b7f
SangKyoungPark <tltmzh3@gmail.com>
2022-05-11 18:47:05 +0900

    [Commit] Partial class

---
+++ Partial/Player.Fight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 클래스를 두개로 쪼갠거임
+// 어쨋던 하나로 합쳐짐
+partial class Player
+{
+    public void Damage()
+    {
+
+    }
+}
+
