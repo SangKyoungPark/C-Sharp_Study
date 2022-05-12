4dc4ec905f3c37f212e3c640ce3a176d0fa89fdb
SangKyoungPark <tltmzh3@gmail.com>
2022-05-12 17:08:49 +0900

    [Commit] Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 사용자 정의 자료형인데,
+interface QuestUnit
+{
+    // 멤버변수 X
+    // int a = 10;
+
+    // 함수의 형태만 물려 줄 수 있는 문법이다.
+    // 함수 선언만 할 수 있고 정의는 할 수 없다.
+    // 따라서, Private 사용 불가 -> 물려주는 의도가 있기 때문
+    // 하지만, 상속의 개념보다는 포함의 개념이다.
+    void Talk(QuestUnit _OtherUnit);
+    void Event(QuestUnit _OtherUnit);
+}
+
+class FightUnit
+{
+    int AT;
+    int DMG;
+
+    public void Damage()
+    {
+
+    }
+}
+
+class Player : FightUnit, QuestUnit
+{
+
+    public void Talk(QuestUnit _OtherUnit)
+    {
+
+    }
+    public void Event(QuestUnit _OtherUnit)
+    {
+
+    }
+}
+
+class Monster : FightUnit, QuestUnit
+{
+    public void Talk(QuestUnit _OtherUnit)
+    {
+
+    }
+    public void Event(QuestUnit _OtherUnit)
+    {
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player();
+    }
+}
